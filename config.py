30b974c468e08af72a1d4d59d6b26fd298cdf209
w. ian douglas <ian.douglas@iandouglas.com>
2024-06-17 11:51:32 -0600

    Update config.py to fetch details from ENV

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-username = ''
-password = ''
-hostname = ''
-database = ''
+import os
+
+username = os.getenv('PG_USERNAME')
+password = os.getenv('PG_PASSWORD')
+hostname = os.getenv('PG_HOSTNAME')
+database = os.getenv('PG_DATABASE')
 
 PG_URL = f"postgres://{username}:{password}@{hostname}/{database}"
